05320d34a6509287e5e1b5c4aaaca530253abc32
Chidera Ozoh <Chideraozoh@icloud.com>
2023-05-23 15:07:38 +0100

    initial init of project

---
+++ README.md
@@ -0,0 +1,4 @@
+## MERN PROJECT - MEMORIES APP
+
+---
+A MERN project for learning purposes. 
---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+const DB_URL = 'mongodb+srv://admin:f8v4YhZIJDfZMxOP@hiccapi.ju0gr0c.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+        .catch((err) => console.log(err.message)); 
